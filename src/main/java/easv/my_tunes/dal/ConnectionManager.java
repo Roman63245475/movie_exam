1ef42dc8e275a7d92cb6cbbacf1de83eb64ec811
mirekondro1 <mirekondro@post.cz>
2025-12-08 22:59:45 +0100

    Update ConnectionManager.java
    This reverts commit e7d1f1fd67dc7ccaebb14ecebb05721507490c4f.

--- src/main/java/easv/my_tunes/dal/ConnectionManager.java
+++ src/main/java/easv/my_tunes/dal/ConnectionManager.java
@@ -11,8 +11,8 @@ public class ConnectionManager {
         SQLServerDataSource ds;
         ds = new SQLServerDataSource();
         ds.setDatabaseName("my_tunes_db"); // make this unique as names are shared on server
-        ds.setUser("CS2025b_e_25"); // Use your own username
-        ds.setPassword("CS2025bE25#23"); // Use your own password
+        ds.setUser("xxx"); // Use your own username
+        ds.setPassword("xxx"); // Use your own password
         ds.setServerName("10.176.111.34");
         ds.setPortNumber(1433);
         ds.setTrustServerCertificate(true);
